63f8167b1836ac3d6d9d817fdcde60495078023d
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-06-26 11:03:36 +0200

    Adding init commit for the data.h this is where we will hold all the data that we need. Adding the structure that will contain all the necessary data

---
+++ include/data.h
@@ -0,0 +1,17 @@
+#ifndef DATA_H
+#define DATA_H 
+
+typedef struct {
+    char fips[10];                      // FIPS code (usually empty)
+    char admin2[100];                   // county/admin level 2
+    char province_state[100];           // state/province
+    
+    
+
+
+    
+}
+
+
+
+#endif
